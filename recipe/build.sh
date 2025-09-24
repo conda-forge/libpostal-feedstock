793d6ccdcd2852b93350155822ef1ce744578475
Tim D. Smith <git@tim-smith.us>
2025-09-23 20:13:00 -0700

    Remove upstreamed build changes

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,5 @@
 ./bootstrap.sh
-./configure --datadir=$PREFIX/share/libpostal_data --prefix=$PREFIX $SSE_FLAG
+./configure --datadir=$PREFIX/share/libpostal_data --prefix=$PREFIX
 
 make -j${CPU_COUNT}
 make install
